6931d947bea2f035d54888a31427c4038009ffd7
Manuel Aguilar <manuelaguilar5219@my.whccd.edu>
2025-01-15 19:58:57 -0800

    Add project files.

---
+++ COMP003A.LectureActivity/Program.cs
@@ -0,0 +1,13 @@
+﻿// namespace level
+namespace COMP003A.LectureActivity
+{
+    // class level
+    internal class Program
+    {
+        // main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
